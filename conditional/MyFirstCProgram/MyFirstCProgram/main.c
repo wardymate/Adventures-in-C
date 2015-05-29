09b710031f66035d72c4e0675b0f472bbe67db9e
wardymate <chriswarddev@gmail.com>
2015-05-29 09:00:38 +0100

    added conditionals

---
+++ conditional/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  MyFirstCProgram
+//
+//  Created by turner on 9/26/13.
+//  Copyright (c) 2013 treehouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 11;
+    int b = 13;
+    int minimum;
+    char min_value;
+    
+    if (a < b){
+        minimum = a;
+        min_value = 'a';
+    } else {
+        minimum = b;
+        min_value = 'b';
+    }
+    printf("%c is minimum %d\n", min_value, minimum);
+    return 0;
+}
+
